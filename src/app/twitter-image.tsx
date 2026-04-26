4027ac1f39476f9f77b8042af629c85ce6dc4e4f
prodbyeagle <heypanteryt@gmail.com>
2026-04-26 22:25:01 +0200

    fix: inline twitter-image config (re-export of runtime not allowed)

--- src/app/twitter-image.tsx
+++ src/app/twitter-image.tsx
@@ -1,1 +1,12 @@
-export { default, alt, size, contentType, runtime } from './opengraph-image';
+import OpenGraphImage, {
+	alt as ogAlt,
+	size as ogSize,
+	contentType as ogContentType,
+} from './opengraph-image';
+
+export const runtime = 'edge';
+export const alt = ogAlt;
+export const size = ogSize;
+export const contentType = ogContentType;
+
+export default OpenGraphImage;
